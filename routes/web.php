7f5c19e265bd43be273a3f7882c911e29472827e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-18 15:22:36 +0200

    added route admin restaurant

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\RestauranController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin')->group(function () {
+
+        // Rotte Admin Restaurant CRUD
+        Route::get('/restaurants/create', [RestauranController::class, 'create'])->name('restaurants.create');
+        Route::get('/restaurants', [RestauranController::class, 'create'])->name('restaurants.store');
+        Route::get('/restaurants/{restaurant}', [RestauranController::class, 'show'])->name('restaurants.show');
+        Route::get('/restaurants/{restaurant}/edit', [RestauranController::class, 'edit'])->name('restaurants.edit');
+        Route::get('/restaurants/{restaurant}', [RestauranController::class])->name('restaurants.update');
+    });
 });
 
 require __DIR__ . '/auth.php';
