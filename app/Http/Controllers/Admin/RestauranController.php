fd9133fd8d3c65427a5894c6781e5d350afcdddf
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-19 21:59:00 +0200

    added action update

--- app/Http/Controllers/Admin/RestauranController.php
+++ app/Http/Controllers/Admin/RestauranController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class RestauranController extends Controller
 {
@@ -83,8 +84,41 @@ class RestauranController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Restaurant $restaurant)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|min:5|max:50',
+            'address' => 'required|string|min:5|max:50',
+            'phone' => 'string|min:10|max:15|nullable',
+            'email' => 'email|string|lowercase|nullable',
+            'vat' => ['required', 'string', 'min:13', 'max:13', Rule::unique('restaurants')->ignore($restaurant->vat)],
+            'category_id' => 'required',
+            'image' => 'nullable|url'
+        ], [
+            'name.required' => 'Il nome del ristorante è obbligatorio',
+            'name.min' => 'Il nome non può essere più corto di :min caratteri',
+            'name.max' => 'Il nome non può essere più corto di :max caratteri',
+            'address.required' => 'L\'indirizzo del ristorante è obbligatorio',
+            'address.min' => 'L\'indirizzo del ristorante non può contenere meno di :min caratteri',
+            'address.max' => 'L\'indirizzo del ristorante non può contenere più di :max caratteri',
+            'phone.min' => 'Il numero di telefono non può avere meno di :min cifre',
+            'phone.max' => 'Il numero di telefono non può avere più di :max cifre',
+            'email.email' => 'L\'email inserita non è valida',
+            'email.lowercase' => 'L\'email non può contenere lettere maiuscole',
+            'vat.required' => 'La P.IVA è obbligatoria',
+            'vat.unique' => 'P.IVA già usata',
+            'vat.min' => 'La P.IVA non può contenere meno di :min cifre',
+            'vat.max' => 'La P.IVA non può contenere più di :max cifre',
+            'category_id.required' => 'Categoria obbligatoria',
+            'image.url' => 'Devi inserire un url'
+        ]);
+
+        $data = $request->all();
+
+        $restaurant->slug = Str::slug($data['name']);
+
+        $restaurant->update($data);
+
+        return to_route('admin.restaurants.show', $restaurant->id);
     }
 }
