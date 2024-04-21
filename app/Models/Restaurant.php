bfb38aae99263a222630295a843ee7dc05fa095d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-21 18:46:29 +0200

    added fillable

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -9,6 +9,8 @@ class Restaurant extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'address', 'phone', 'email', 'vat', 'image', 'categories'];
+
     public function dishes()
     {
         return $this->hasMany(Dish::class);
