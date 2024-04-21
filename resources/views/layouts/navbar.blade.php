898f31d3338e6cfdc8145f4aeb66dbb79b5f2f66
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-21 23:13:26 +0200

    changed dropdown class name

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -38,7 +38,7 @@
                         {{ Auth::user()->name }}
                     </a>
 
-                    <div id="ciao" class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <div id="glass-dropdown" class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
                         <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
