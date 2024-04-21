9dd775d0466874b04bef70c615c3e29ccc32b6c4
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-21 14:43:14 +0200

    removed section title

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.app')
 @section('content')
     <section id="restaurant-show">
-        <h1 class="text-center my-4">{{$restaurant->name}}</h1>
-        <div class="card">
+        <div class="card my-4">
+            <div class="card-header">
+                <h1 class="text-center">{{$restaurant->name}}</h1>
+            </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-4">
-                        <img src="" alt="{{$restaurant->name}}">
+                        <img src="{{$restaurant->image}}" alt="{{$restaurant->name}}">
                     </div>
                     <div class="col">
-                        <h3 class="text-center">{{$restaurant->name}}</h3>
                         <p><i class="fa-solid fa-map-pin me-3"></i>{{$restaurant->address}}</p>
                         <p><i class="fa-solid fa-phone me-3"></i>{{$restaurant->phone}}</p>
                         <p><i class="fa-solid fa-location-dot me-3"></i>{{$restaurant->city}}</p>
@@ -18,8 +19,11 @@
                     </div>
                 </div>
             </div>
-            <div class="card-footer mt-4">
-                <a href="{{route('admin.restaurants.edit', $restaurant->id)}}" class="btn btn-warning"><i class="fas fa-pencil me-2"></i>Modifica</a>
+            <div class="card-footer">
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="" class="btn btn-secondary"><i class="fa-solid fa-left-long me-2"></i>Torna indietro</a>
+                    <a href="{{route('admin.restaurants.edit', $restaurant->id)}}" class="btn btn-warning"><i class="fas fa-pencil me-2"></i>Modifica</a>
+                </div>
             </div>
         </div>
     </section>
