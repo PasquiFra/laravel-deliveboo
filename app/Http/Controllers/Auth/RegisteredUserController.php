63ad3f09aad590afc379b79d1f0523e5b2646ef8
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-21 23:12:14 +0200

    added validate

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,28 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'lastname' => 'required|string|max:255',
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'telephone_number' => 'string|min:10|max:15',
+            'birthday' => 'date|nullable',
+        ], [
+            'name.required' => 'Il campo Nome è un campo obbligatorio',
+            'lastname.required' => 'Il campo Cognome è obbligatorio',
+            'email.required' => 'Il campo Email è obbligatorio',
+            'email.lowercase' => 'Nel campo Email non possono essere inserite lettere maiuscole',
+            'email.unique' => 'Email già utilizzata',
+            'telephone_number.min' => 'Il numero di telefono non può avere meno di :min cifre',
+            'telephone_number.max' => 'Il numero di telefono non può avere più di :max cifre'
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'lastname' => $request->lastname,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'telephone_number' => $request->telephone_number,
+            'birthday' => $request->birthday,
         ]);
 
         event(new Registered($user));
