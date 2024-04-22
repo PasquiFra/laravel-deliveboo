4d013241dff2c630c93b0716639a10c368e4b998
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-22 16:30:35 +0200

    responsive show

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 <section class="p-3 my-5">
-    <div class="card-show px-4 pb-4 card-color">
+    <div class="card-show px-4 pb-4 card-color">    
         <div class="card-header pt-3 pb-4">
             <div class="col">
                 <h1 class="card-title text-center">{{$dish->name}}</h1>
             </div>
         </div>
-        <div class="card-body row position-relative"> 
-            <div class="col-3">
-                <picture id="show-picture">
+        <div class="card-body row position-relative d-flex align-items-center"> 
+
+            {{-- IMMAGINE --}}
+            <div class="col-sm-auto col-md-6 col-lg-4 col-xl-3 col-12">
+                <picture id="show-picture text-center">
                     @if ($dish->image && @getimagesize($dish->image))
                         <img src="{{$dish->image}}" alt="foto-{{$dish->slug}}" class="img-fluid rounded">
                     @else
@@ -18,9 +20,11 @@
                     @endif
                 </picture>
             </div>
-            <div class="col d-flex justify-content-between"> 
+
+            {{-- INFO PIATTO --}}
+            <div class="col-sm-12 col-md-6 col-lg-8 col-xl-9 col d-flex justify-content-between"> 
                 <div class="info-dishes">
-                    <div class="mb-3">
+                    <div class="mb-3 mt-3">
                         L'articolo è
                         <span class="{{$dish->availability ? 'available' : 'text-disabled'}}">
                             <strong>
@@ -31,12 +35,12 @@
                     <div class="d-flex gap-2">
                         <div class="mb-3">
                             @if ($dish->course)
-                                <span class="tag bg-red text-center">{{$dish->course}}</span>
+                                <span class="tag bg-course px-2 py-1 text-center">{{$dish->course}}</span>
                             @endif
                         </div>
                         <div class="mb-3">
                             @if ($dish->diet)
-                                <span class="tag bg-yellow text-center">{{$dish->diet}}</span>
+                            <span class="tag bg-yellow px-2 py-1 text-center">{{$dish->diet}}</span>
                             @endif
                         </div>
                     </div>
@@ -54,7 +58,7 @@
                 </div>
                 {{-- BOTTONI di CRUD --}}
                 <div class="button-group ">
-                    <div class="d-flex gap-2">
+                    <div class="d-flex gap-2 mt-4">
                         {{--# EDIT --}}
                         <a href="{{ route('admin.dishes.edit', $dish->id)}}" class="btn btn-outline-light edit">
                             <i class="fas fa-pencil"></i>
