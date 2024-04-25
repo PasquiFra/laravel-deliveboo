8ca8d001637c8cf4e6eb1d811552b1a5e78aa5c0
Your Name <you@example.com>
2024-04-25 17:09:46 +0200

    Created routes/checkout.js, filled it with payment logic

---
+++ braintreeTutorial/routes/checkout.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+    const gateway = new braintree.BraintreeGateway({
+        environment: braintree.Environment.Sandbox,
+        // Use your own credentials from the sandbox Control Panel here
+        merchantId: 'n6bdrym272w78zgq',
+        publicKey: '93qwd4k2x8fw5jmn',
+        privateKey: 'c5800d51357f1d9ddde252715e87a327'
+    });
+
+    // Use the payment method nonce here
+    const nonceFromTheClient = req.body.paymentMethodNonce;
+    // Create a new transaction for $10
+    const newTransaction = gateway.transaction.sale({
+        amount: '10.00',
+        paymentMethodNonce: nonceFromTheClient,
+        options: {
+            // This option requests the funds from the transaction
+            // once it has been authorized successfully
+            submitForSettlement: true
+        }
+    }, (error, result) => {
+        if (result) {
+            res.send(result);
+        } else {
+            res.status(500).send(error);
+        }
+    });
+});
+
+module.exports = router;
