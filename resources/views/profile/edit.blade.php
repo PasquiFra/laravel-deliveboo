4d355ccce0aded9367b9cb75988f5a104f10543f
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-26 23:31:00 +0200

    added style in profile information card

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,10 +5,10 @@
 @section('content')
 
 <div class="container spacing">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Profile') }}
+    <h2 class="fs-4 text-black my-4">
+        {{ __('Profilo') }}
     </h2>
-    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+    <div class="glass-card p-4 mb-4 shadow rounded-lg">
 
         @include('profile.partials.update-profile-information-form')
 
