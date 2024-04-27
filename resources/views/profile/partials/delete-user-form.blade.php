fbf73beac1f5078f9b4eb49d5a99eb3e28392ee8
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-28 00:23:04 +0200

    updated style of delete account modal

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -18,21 +18,21 @@
     <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
     <div class="modal fade" id="delete-account" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="delete-account" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-            <div id="glass-modal" class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="delete-account">Delete Account</h5>
+            <div id="glass-modal" class="modal-content p-4">
+                <div class="d-flex justify-content-between align-items-center ms-3">
+                    <h5 class="modal-title" id="delete-account">Elimina Account</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <h2 class="text-lg font-medium text-gray-900">
-                        {{ __('Are you sure you want to delete your account?') }}
+                    <h2 class="text-lg font-medium text-gray-900 mb-3">
+                        {{ __('Sei sicuro/a di volere eliminare il tuo account?') }}
                     </h2>
-                    <p class="mt-1 text-sm text-gray-600">
-                        {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                    <p class="mt-2 text-sm text-gray-600">
+                        {{ __('Dopo l\'eliminazione dell\'account, tutte le sue risorse e dati saranno cancellati in modo permanente. Ti preghiamo di inserire la password per confermare definitivamente l\'eliminazione dell\'account.') }}
                     </p>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <div class="d-flex justify-content-between align-items-center ms-3">
+                    <button type="button" class="btn-outline-index text-white fw-semibold gray ms-1 px-3 py-2 rounded-pill d-flex align-items-center text-white fw-semibold" data-bs-dismiss="modal">Torna indietro</button>
 
                     <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
                         @csrf
@@ -41,7 +41,7 @@
 
                         <div class="input-group">
 
-                            <input id="password" name="password" type="password" class="form-control" placeholder="{{ __('Password') }}" />
+                            <input id="password" name="password" type="password" class="form-control me-2 bg-transparent border-dark-light rounded-pill" placeholder="{{ __('Password') }}" />
 
                             @error('password')
                             <span class="invalid-feedback mt-2" role="alert">
@@ -51,8 +51,8 @@
 
 
 
-                            <button type="submit" class="btn btn-danger">
-                                {{ __('Delete Account') }}
+                            <button type="submit" class="btn-outline-index text-white fw-semibold red ms-1 px-3 py-2 rounded-pill">
+                                {{ __('Elimina Account') }}
                             </button>
                             <!--  -->
                         </div>
