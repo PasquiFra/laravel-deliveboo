8f93688fc0e3dacbdc9bbcce9315e6f7bf27727c
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-28 15:58:13 +0200

    imported correct controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\DishController;
-use App\Http\Controllers\Api\CategoryRestaurantController;
 use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\CategoryRestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
