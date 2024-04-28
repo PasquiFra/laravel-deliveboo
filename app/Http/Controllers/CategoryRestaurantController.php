b65f824852c5bf227b4b7a22c1ad592258ef65b6
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-28 12:18:38 +0200

    filled CategoryRestaurantController

--- app/Http/Controllers/CategoryRestaurantController.php
+++ app/Http/Controllers/CategoryRestaurantController.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryRestaurantController extends Controller
 {
-    //
+    public function __invoke(string $id)
+    {
+        $category = Category::find($id);
+        if (!$category) return response(null, 404);
+        $category->load('restaurants.categories');
+        $restaurants = $category->restaurants;
+        $categories = Category::all();
+        return response()->json(['restaurants' => $restaurants, 'categories' => $categories]);
+    }
 }
