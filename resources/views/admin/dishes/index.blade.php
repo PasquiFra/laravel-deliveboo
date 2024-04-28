b04802e81e29731d80c2a1ed6f770cb7bd50afa5
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-04-28 15:57:30 +0200

    updated responsive table

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -7,9 +7,10 @@
 <div class="mb-2">
   <div class="mb-3 d-flex justify-content-between align-items-center spacing">
     <!--Filtro per Portata del Piatto-->
-    <form class="filter-courses" action="{{ route('admin.dishes.index') }}" method="GET">
-      <div class="input-group">
-          <select role="button" id="coruse-filter" class="form-select w-25 text-white fw-bold bg-transparent rounded-pill" name="course">
+    <form class="filter-courses d-flex align-items-center justify-content-center" action="{{ route('admin.dishes.index') }}" method="GET">
+      <div class="input-group d-flex flex-column flex-lg-row gap-2">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-5 mb-1">
+          <select role="button" id="coruse-filter" class="form-select text-white fw-bold bg-transparent rounded-pill" name="course">
             <option class="fw-semibold" value="">Tutte le portate</option>
             @foreach ($courses as $course)
             <option class="fw-semibold" value="{{ $course }}"  {{ request('course') == $course ? 'selected' : '' }}>
@@ -17,24 +18,32 @@
             </option>
             @endforeach
           </select>
+        </div>
+        <div class="col-12 flex-lg-row col-sm-12 col-md-12 col-lg-4 gap-2">
           <!--Filtro in base a disponibilità del piatto-->
-          <select id="availability-filter" role="button" class="form-select w-25 pe-5 text-white fw-bold bg-transparent ms-2 rounded-pill" name="availability"> 
+          <select id="availability-filter" role="button" class="form-select pe-5 text-white fw-bold bg-transparent rounded-pill" name="availability"> 
             <option value="">Tutti</option>
             <option value="available" @if($availability==='available')selected @endif>Disponibile</option>
             <option value="not-available" @if($availability==='not-available')selected @endif>Non Disponibile</option>
           </select>
-          <button class="btn-outline-index text-white fw-semibold rounded-pill gray ms-2" type="submit">Filtra</button>
+        </div>
+        <div class="col-2 text-center">
+          <button class="btn-outline-index text-white my-1 fw-semibold rounded-pill px-2 py-1 gray" type="submit">Filtra</button>
+        </div>
       </div>
-  </form>
-    <h1 class="text-white me-5">Menu</h1>
+    </form>
+    
+    <h1 class="text-white me-4">Menu</h1>
     <div class="d-flex justify-content-end gap-2 p-2">
       <!-- Cestino -->
-      <a href="{{route('admin.dishes.trash')}}" class="btn-outline-index red text-white fw-semibold ms-1 px-3 py-2 rounded-pill">
-          <i class="far fa-trash-can"></i> Vedi Cestino
+      <a href="{{route('admin.dishes.trash')}}" class="btn-outline-index red text-white fw-semibold ms-1 px-3 py-1 rounded-pill">
+          <i class="far fa-trash-can"></i>
+          <span class="d-none d-xl-inline">Vedi Cestino</span>
       </a>
       <!-- Crea nuovo dish -->
-      <a href="{{route('admin.dishes.create')}}" class="btn-outline-index text-white fw-semibold green ms-1 px-3 py-2 rounded-pill">
-        <i class="fa-solid fa-plus"></i> Aggiungi piatto
+      <a href="{{route('admin.dishes.create')}}" class="btn-outline-index text-white fw-semibold green ms-1 px-3 py-1 rounded-pill">
+        <i class="fa-solid fa-plus"></i>
+        <span class="d-none d-xl-inline">Aggiungi piatto</span>
       </a>
     </div>
   </div>
@@ -42,11 +51,11 @@
     <table class="text-center">
       <thead>
         <tr>
-          <th>Immagine</th>
+          <th class="d-none d-lg-table-cell">Immagine</th>
           <th>Nome</th>
           <th>Online</th>
-          <th>Portata</th>
-          <th>Dieta</th>
+          <th class="d-none d-lg-table-cell">Portata</th>
+          <th class="d-none d-lg-table-cell">Dieta</th>
           <th>Prezzo</th>
           <th>Ultima modifica</th>
           <th></th>
@@ -59,7 +68,7 @@
       <tbody>
         @forelse ($dishes as $dish)
           <tr>
-            <td class="text-center d-flex justify-content-center">
+            <td class="text-center d-flex justify-content-center d-none d-lg-table-cell">
               <div class="index-prev">
                 @if ($dish->image)
                     <img src="{{asset('storage/' . $dish->image)}}" alt="foto-{{$dish->slug}}" class="show-image">
@@ -79,11 +88,11 @@
               @endif
             </td>
             {{--Portata--}}
-            <td class="text-center">
+            <td class="text-center d-none d-lg-table-cell">
               {{$dish->course}}
             </td>
             {{--Dieta--}}
-            <td class="text-center">
+            <td class="text-center d-none d-lg-table-cell">
               @if($dish->diet)
                {{$dish->diet}}
               @else
@@ -94,7 +103,7 @@
             <td class="text-center">{{$dish->price}} €</td>
             <td class="text-center">{{$dish->updated_at}}</td>
             <td>
-              <div class="d-flex gap-2 justify-content-end">
+              <div class="d-flex gap-2 flex-column flex-xl-row align-items-center justify-content-end">
                 {{--# COLLEGAMENTO A SHOW --}}
                 <a href="{{ route('admin.dishes.show', $dish->id)}}" class="rounded px-2 py-1 btn-outline-index blue text-white fw-semibold">
                   <i class="far fa-eye"></i>
--- resources/views/admin/dishes/trash.blade.php
+++ resources/views/admin/dishes/trash.blade.php
@@ -5,7 +5,10 @@
 @section('content')
 <section id='dishes-trash' class="spacing">
     <div class="mb-2 d-flex justify-content-between align-items-center">
-      <a href="{{route('admin.dishes.index')}}" class="btn-outline-index fw-semibold gray ms-1 px-3 py-2 rounded-pill"><i class="fa-solid fa-arrow-left me-2"></i>Torna indietro</a>
+      <a href="{{route('admin.dishes.index')}}" class="btn-outline-index fw-semibold gray text-white ms-1 px-3 py-1 py-md-2 rounded-pill">
+        <i class="fa-solid fa-arrow-left"></i>
+        <span class="d-none d-md-inline-block">Torna indietro</span>
+      </a>
       <h1 class="text-white text-center mb-0">Piatti Eliminati</h1>
     </div>
     {{--Tabella--}}
@@ -13,10 +16,11 @@
     <table class="text-center">
       <thead>
         <tr>
-          <th>Immagine</th>
+          <th class="d-none d-lg-table-cell">Immagine</th>
           <th>Nome</th>
           <th>Online</th>
-          <th>Dieta</th>
+          <th class="d-none d-lg-table-cell">Portata</th>
+          <th class="d-none d-lg-table-cell">Dieta</th>
           <th>Prezzo</th>
           <th>Ultima modifica</th>
           <th></th>
@@ -29,7 +33,7 @@
       <tbody>
         @forelse ($dishes as $dish)
           <tr>
-            <td class="text-center d-flex justify-content-center">
+            <td class="text-center d-flex justify-content-center d-none d-lg-table-cell">
               <div class="index-prev">
                 @if ($dish->image)
                     <img src="{{asset('storage/' . $dish->image)}}" alt="foto-{{$dish->slug}}" class="show-image">
@@ -38,7 +42,9 @@
                 @endif
               </div>
             </td>
+            {{--Nome--}}
             <td class="text-center">{{$dish->name}}</td>
+            {{--Disponibilità--}}
             <td class="text-center">
               @if ($dish->availability == 1)
                 <span class="stamp is-available"></span> 
@@ -46,17 +52,23 @@
                 <span class="stamp not-available"></span>
               @endif
             </td>
-            <td class="text-center">
+            {{--Portata--}}
+            <td class="text-center d-none d-lg-table-cell">
+              {{$dish->course}}
+            </td>
+            {{--Dieta--}}
+            <td class="text-center d-none d-lg-table-cell">
               @if($dish->diet)
                {{$dish->diet}}
               @else
                 ---
               @endif
             </td>
-            <td class="text-center">{{$dish->price}}</td>
+            {{--Prezzo--}}
+            <td class="text-center">{{$dish->price}} €</td>
             <td class="text-center">{{$dish->updated_at}}</td>
             <td>
-              <div class="d-flex gap-2 justify-content-end">
+              <div class="d-flex gap-2 flex-column flex-xl-row align-items-center justify-content-end">
                 {{--# COLLEGAMENTO A SHOW --}}
                 <a href="{{ route('admin.dishes.show', $dish->id)}}" class="rounded px-2 py-1 btn-outline-index blue">
                   <i class="far fa-eye"></i>
