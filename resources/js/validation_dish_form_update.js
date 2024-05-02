d49ea5fbc2f204ed20735fd4807c6c5810f9b6f5
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-02 19:20:40 +0200

    added dish form update validation

--- resources/js/validation_dish_form_update.js
+++ resources/js/validation_dish_form_update.js
@@ -2,21 +2,25 @@
 const formUpdate = document.getElementById('form-update');
 const inputs = document.querySelectorAll('.form-inputs');
 const invalidMessage = document.querySelectorAll('.invalid-message');
+const dietSelect = document.getElementById('diet');
+const courseSelect = document.getElementById('course');
 
-console.log(formUpdate)
-
-// Flag
-let isValid = true;
+// Regex
+const minRegexDecimal = /^\d+(.\d{1})?$/;
+const maxRegexDecimal = /^\d+.\d{0,2}$/;
 
 //! Effettiva validazione
 formUpdate.addEventListener('submit', e => {
+    // Flag
+    let isValid = true;
+
     // Giro su tutti gli input
     inputs.forEach((input, i) => {
-        e.preventDefault();
 
         const inputField = input.value.trim();
+
         // Se l'input è vuoto
-        if (!inputField) {
+        if (input.value.length === 0) {
 
             // Riassegno la flag a false
             isValid = false;
@@ -38,5 +42,81 @@ formUpdate.addEventListener('submit', e => {
             invalidMessage[i].innerText = '';
         }
 
-    })
+
+        // Input name
+        if (input.id === 'name') {
+            // Se è un numero
+            if (!isNaN(inputField) && inputField.length >= 1) {
+
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il nome del ristorante non può avere solo numeri';
+            }
+        }
+
+        if (input.id === 'price') {
+            if (inputField < 0) {
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il prezzo non può essere negativo';
+            }
+
+            if (inputField.match(minRegexDecimal)) {
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il prezzo deve avere 2 decimali';
+            } else if (!inputField.match(maxRegexDecimal) && inputField.length >= 1) {
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il prezzo non può avere più di 3 decimali';
+            }
+
+        }
+
+    });
+
+    // Validazione diet select
+    if (dietOptions.includes(dietSelect.value) || !dietSelect.value) {
+        dietSelect.classList.remove('is-invalid');
+    } else {
+        isValid = false;
+        dietSelect.classList.add('is-invalid');
+        dietSelect.classList.remove('is-valid');
+    }
+
+    // Validazione course select
+    if (courseOptions.includes(courseSelect.value)) {
+        courseSelect.classList.add('is-valid');
+        courseSelect.classList.remove('is-invalid');
+    } else {
+        isValid = false
+        courseSelect.classList.add('is-invalid');
+        courseSelect.classList.remove('is-valid');
+    }
+
+    if (!isValid) e.preventDefault();
 })
