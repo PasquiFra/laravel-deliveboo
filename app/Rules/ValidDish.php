f5fb70d66ed0cee8e1f86b13503e7170fa219790
Your Name <you@example.com>
2024-05-04 15:51:51 +0200

    comments

--- app/Rules/ValidDish.php
+++ app/Rules/ValidDish.php
@@ -6,7 +6,9 @@ use App\Models\Dish;
 use Closure;
 use Illuminate\Contracts\Validation\Rule;
 
-//In origine era presente Validation/ValidationRule, abbiamo sostituito con Validation/Rule in modo da farci restituire il messaggio di errore del prodotto nel caso di ID del prodotto inesistente
+//In origine era presente Validation/ValidationRule, abbiamo sostituito con Validation/Rule
+// in modo da farci restituire il messaggio di errore del prodotto 
+//nel caso di ID del prodotto inesistente
 class ValidDish implements Rule
 {
     /**
@@ -21,6 +23,7 @@ class ValidDish implements Rule
 
     public function passes($attribute, $value)
     {
+        //vado a prendere l'id del prodotto da aggiungere all'ordine e restituisco un booleano (prodotto trovato/non trovato)
         if (Dish::find($value)) {
             return true;
         }
@@ -29,6 +32,7 @@ class ValidDish implements Rule
 
     public function message()
     {
+        //Se il prodotto non viene trovato restituisco un messaggio
         return 'Il prodotto non esiste';
     }
 }
