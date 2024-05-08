58217058522090afdfebdd638ef19d57bd5346b9
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-05-08 21:40:07 +0200

    fixed order table and modal close button

--- resources/views/layouts/includes/modal.blade.php
+++ resources/views/layouts/includes/modal.blade.php
@@ -9,7 +9,7 @@
         <div class="modal-body text-white py-5 row align-items-center">
         </div>
         <div class="modal-footer justify-content-between py-3 px-4">
-          <button type="button" class="btn-outline-index text-white fw-semibold gray ms-1 px-3 py-2 rounded-pill d-flex align-items-center fw-semibold" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn-outline-index text-white fw-semibold gray ms-1 px-3 py-2 rounded-pill d-flex align-items-center fw-semibold" data-bs-dismiss="modal">Chiudi</button>
           <button type="button" class="btn btn-info" id="modal-confirmation-button">Conferma</button>
         </div>
       </div>
