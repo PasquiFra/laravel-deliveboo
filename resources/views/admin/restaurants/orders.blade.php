c2a4b942377471d98c5085207834b420065b972f
Your Name <you@example.com>
2024-05-08 18:26:38 +0200

    riepilogo ordini ok

--- resources/views/admin/restaurants/orders.blade.php
+++ resources/views/admin/restaurants/orders.blade.php
@@ -15,9 +15,9 @@
                         <th>Data Ordine</th>
                         <th>Nome</th>
                         <th>Cognome</th>
-                        <th class="large-column">Email</th>
+                        <th class="large-column d-none d-lg-table-cell">Email</th>
                         <th>Indirizzo</th>
-                        <th>Numero di Telefono</th>
+                        <th class="d-none d-lg-table-cell">Numero di Telefono</th>
                         <th><strong>Totale ordine<br>€</strong></th>
                     </tr>
                 </thead>
@@ -29,9 +29,9 @@
                         <td>{{$order->created_at}}</td>
                         <td>{{$order->name}}</td>
                         <td>{{$order->lastname}}</td>
-                        <td class="large-column">{{$order->email}}</td>
+                        <td class="large-column d-none d-lg-table-cell">{{$order->email}}</td>
                         <td>{{$order->address}}</td>
-                        <td>{{$order->phone}}</td>
+                        <td class="d-none d-lg-table-cell">{{$order->phone}}</td>
                         <td><strong>{{$order->total}}</strong></td>
                     </tr>
                     @empty
@@ -40,8 +40,8 @@
                     <tr id="total-orders">
                         <td></td>
                         <td></td>
-                        <td></td>
-                        <td></td>
+                        <td class="d-none d-lg-table-cell"></td>
+                        <td class="d-none d-lg-table-cell"></td>
                         <td></td>
                         <td colspan="2" class="text-end">Totale ordini €</td>
                         <td>{{number_format($total_orders, 2)}}</td>
