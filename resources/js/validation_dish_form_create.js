01878490d171b7e8e10b9ff7afec22141bc3a06e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-16 19:05:59 +0200

    fixed validation

--- resources/js/validation_dish_form_create.js
+++ resources/js/validation_dish_form_create.js
@@ -5,15 +5,103 @@ const invalidMessage = document.querySelectorAll('.invalid-message');
 const dietSelect = document.getElementById('diet');
 const courseSelect = document.getElementById('course');
 
+// Flag
+let isValid = false;
 
 // Regex
 const minRegexDecimal = /^\d+(.\d{1})?$/;
 const maxRegexDecimal = /^\d+.\d{0,2}$/;
 
 //! Effettiva validazione
+
+// Validazione al blur
+inputs.forEach((input, i) => {
+    input.addEventListener('blur', () => {
+        const inputField = input.value.trim();
+
+        // Se l'input è vuoto
+        if (input.value.length === 0) {
+
+            // Riassegno la flag a false
+            isValid = false;
+
+            // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+            input.classList.add('is-invalid');
+            input.classList.remove('is-valid');
+
+            // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+            invalidMessage[i].innerText = 'Il campo è obbligatorio'
+
+        } else {
+
+            // Aggiungo la classe 'is-valid' e rimuovo la classe 'is-invalid'
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+
+            // Rimuovo il messaggio di errore
+            invalidMessage[i].innerText = '';
+        }
+
+
+        // Input name
+        if (input.id === 'name') {
+            // Se è un numero
+            if (!isNaN(inputField) && inputField.length >= 1) {
+
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il nome del ristorante non può avere solo numeri';
+            }
+        }
+
+        if (input.id === 'price') {
+            if (input.value <= 0) {
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = input.value < 0 ? 'Il prezzo non può essere negativo' : 'Il prezzo non può essere 0';
+
+            } else if (inputField.match(minRegexDecimal)) {
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il prezzo deve avere 2 decimali';
+            } else if (!inputField.match(maxRegexDecimal) && inputField.length >= 1) {
+                // Riassegno la flag a false
+                isValid = false;
+
+                // Aggiungo la classe 'is-invalid' e rimuovo la classe 'is-valid'
+                input.classList.add('is-invalid');
+                input.classList.remove('is-valid');
+
+                // Costruisco il messaggio di errore e lo aggiungo all'invalid message
+                invalidMessage[i].innerText = 'Il prezzo non può avere più di 3 decimali';
+            }
+
+        }
+    })
+})
+
+// Validazione al submit
 formCreate.addEventListener('submit', e => {
-    // Flag
-    let isValid = true;
+    // Setto la Flag a true
+    isValid = true;
 
     // Giro su tutti gli input
     inputs.forEach((input, i) => {
